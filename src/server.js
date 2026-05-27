35f8cbc4082b68e8147d1650ff314d2aeff37af3
Sarojsahoo23 <149657691+Sarojsahoo23@users.noreply.github.com>
2026-05-27 13:56:47 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,12 +4,29 @@ const app = require("./app");
 
 const connectDB = require("./config/db");
 
-connectDB();
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(
-    `Server running on port ${PORT}`
-  );
-});
+const startServer = async () => {
+  try {
+
+    // CONNECT DATABASE
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(
+        `Server running on port ${PORT}`
+      );
+    });
+
+  } catch (error) {
+
+    console.log(
+      "Server Error:",
+      error.message
+    );
+
+    process.exit(1);
+  }
+};
+
+startServer();
