76408ad81c6cf52540f77042d45a1297b38ca90c
Sarojsahoo23 <149657691+Sarojsahoo23@users.noreply.github.com>
2026-05-27 20:47:56 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,23 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.MONGO_URI);
 
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 30000,
-    });
-
-    console.log(
-      `MongoDB Connected: ${conn.connection.host}`
-    );
-
+    console.log(" MongoDB Connected");
   } catch (error) {
-
-    console.error(
-      "MongoDB Connection Error:",
-      error
-    );
-
+    console.error(" MongoDB Connection Error:", error.message);
     process.exit(1);
   }
 };
