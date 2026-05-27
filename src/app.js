f88789ab74a49d6d936311fb8eb2292449593b8a
Sarojsahoo23 <149657691+Sarojsahoo23@users.noreply.github.com>
2026-05-27 20:34:41 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,6 +17,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:3000",
       "https://loan-app-fronted-app.vercel.app",
+      "https://loan-app-backend-ecru.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
